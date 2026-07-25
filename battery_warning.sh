181283e4f87abd2b8b8637aec66cb845adc0ffde
nucakusiani <yourrlilfreak@gmail.com>
2026-07-25 17:14:00 +0400

    initial commit

---
+++ battery_warning.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+while true; do
+  battery=$(acpi -b | grep -oP '\d+(?=%)')
+  if ((battery <= 20)); then
+    notify-send "Warning!" "The battery percentage is below 20!"
+  fi
+  sleep 60
+done
